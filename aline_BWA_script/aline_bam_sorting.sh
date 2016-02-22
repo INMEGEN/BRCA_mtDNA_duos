2973a559e4b80b1121dd6253c296685abc35022c
hachepunto <hachepunto@gmail.com>
2016-02-22 15:04:32 -0600

    Correct script

--- aline_BWA_script/aline_bam_sorting.sh
+++ aline_BWA_script/aline_bam_sorting.sh
@@ -1,18 +1,21 @@
 # step 1. Indexing reference
 bwa index ~/mamaMit/references/NC_012920.1.fasta
+samtools faidx ~/mamaMit/references/NC_012920.1.fasta
 
-# step 2. Aline fasta files
-# the folder "BRCaMt" contain all the raw fastq.gz 
+step 2. Aline fasta files
+the folder "BRCaMt" contain all the raw fastq.gz 
 cd /home/hachepunto/mamaMit/BRCaMt/
 R1=( `ls *R1_001.fastq.gz` )
 R2=( `ls *R2_001.fastq.gz` )
-for i  in `seq ${#R1[@]}` ;
+for i  in `seq ${#R1[@]}`;
 do
-        bwa   mem   /home/hachepunto/mamaMit/references/NC_012920.1.fasta \
-                ${R1[$i]} \
-                ${R2[$i]} \
+        bwa   mem -t 10  /home/hachepunto/mamaMit/references/NC_012920.1.fasta \
+                ${R1[$i -1]} \
+                ${R2[$i -1]} \
                 > ../sam/${R1[$i]%_L001_R1_001.fastq.gz}.sam;
 done
+
+
 
 #step 3. Convert from sam to bam
 for file in `ls /home/hachepunto/mamaMit/sam`; 
