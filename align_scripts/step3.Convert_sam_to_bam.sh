2849f09f0742ac1c5060650f2c914cf72219ee5e
hachepunto <hachepunto@gmail.com>
2016-02-23 12:25:43 -0600

    patch paths

--- align_scripts/step3.Convert_sam_to_bam.sh
+++ align_scripts/step3.Convert_sam_to_bam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #step 3. Convert from sam to bam
-for file in `ls /home/hachepunto/mamaMit/sam`; 
+cd ../data/sam
+for file in `ls *sam`; 
 do
-    
-    samtools  view -h -b -S   /home/hachepunto/mamaMit/sam/$file \
-	      -o /home/hachepunto/mamaMit/bam/${file%.sam}.bam &
+    samtools  view -h -b -S $file \
+	      -o ../bam/${file%.sam}.bam &
 done
