1fee4eb7c7e630e18d3cac79d305b0c09ca917c7
hachepunto <hachepunto@gmail.com>
2016-02-23 13:11:24 -0600

    replace contig name for MT

--- reference/get_Mt_reference.sh
+++ reference/get_Mt_reference.sh
@@ -1,1 +1,3 @@
-wget "http://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&sendto=on&log$=seqview&db=nuccore&dopt=fasta&sort=&val=251831106&from=begin&to=end&extrafeat=976, http://www.ncbi.nlm.nih.gov/nuccore/NC_012920" -O NC_012920.fasta
+echo ">MT" > NC_012920.fasta
+curl "http://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&sendto=on&log$=seqview&db=nuccore&dopt=fasta&sort=&val=251831106&from=begin&to=end&extrafeat=976, http://www.ncbi.nlm.nih.gov/nuccore/NC_012920" | tail -n +2 >> NC_012920.fasta
+
