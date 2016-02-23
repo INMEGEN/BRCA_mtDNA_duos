5396f4dd392b450472311be866c1275139203918
hachepunto <hachepunto@gmail.com>
2016-02-23 12:54:57 -0600

    correct sequence

--- align_scripts/step2.Align_fasta_files.sh
+++ align_scripts/step2.Align_fasta_files.sh
@@ -9,10 +9,10 @@ cd ../data/fastq
 R1=( `ls *R1_001.fastq.gz` )
 R2=( `ls *R2_001.fastq.gz` )
 
-for i  in `seq ${#R1[@]}`;
+for i  in `seq 0 ${#R1[@]}`;
 do
     bwa mem -t $THREADS  ../../reference/NC_012920.fasta \
-        ${R1[$i -1]} \
-        ${R2[$i -1]} \
+        ${R1[$i]} \
+        ${R2[$i]} \
         > ../sam/${R1[$i]%_L001_R1_001.fastq.gz}.sam;
 done
