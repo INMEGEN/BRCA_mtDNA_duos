d308e497846121e805d31aa45c8e524614d32af3
hachepunto <hachepunto@gmail.com>
2016-02-23 12:21:48 -0600

    patch paths

--- align_scripts/step2.Align_fasta_files.sh
+++ align_scripts/step2.Align_fasta_files.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+THREADS=24
+
 #step 2. Aline fasta files
-the folder "BRCaMt" contain all the raw fastq.gz 
-cd /home/hachepunto/mamaMit/BRCaMt/
+# the folder "BRCaMt" contain all the raw fastq.gz 
+cd ../data/fastq
+
 R1=( `ls *R1_001.fastq.gz` )
 R2=( `ls *R2_001.fastq.gz` )
+
 for i  in `seq ${#R1[@]}`;
 do
-        bwa   mem -t 10  /home/hachepunto/mamaMit/references/NC_012920.1.fasta \
-                ${R1[$i -1]} \
-                ${R2[$i -1]} \
-                > ../sam/${R1[$i]%_L001_R1_001.fastq.gz}.sam;
+    bwa mem -t $THREADS  ../../reference/NC_012920.fasta \
+        ${R1[$i -1]} \
+        ${R2[$i -1]} \
+        > ../sam/${R1[$i]%_L001_R1_001.fastq.gz}.sam;
 done
