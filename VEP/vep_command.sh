02caab0dcad4f410db2f21a25eb5e58508cbc7b7
hachepunto <hachepunto@gmail.com>
2016-06-21 15:21:00 -0500

    strelka results VEP analysis

--- VEP/vep_command.sh
+++ VEP/vep_command.sh
@@ -22,3 +22,28 @@ perl variant_effect_predictor.pl \
 	-i /proyectos/home/hachepunto/BRCA_mtDNA_duos/data/full_variants/merge_54pairs.vcf.gz \
 	-o /proyectos/home/hachepunto/BRCA_mtDNA_duos/VEP/merge_54pairs_VEP.vcf \
 	--stats_file /proyectos/home/hachepunto/BRCA_mtDNA_duos/VEP/merge_54pairs_vep.vcf_summary.html
+
+
+# Strelka results VEP 
+
+# Strelka all.somatic.snvs merge
+
+perl variant_effect_predictor.pl \
+	--everything \
+	--cache \
+	--vcf \
+	--port 3337 \
+	-i /proyectos/home/hachepunto/BRCA_mtDNA_duos/strelka/strelka_all.somatic_results/mergevcf/strelka_all.somatic_merge.vcf.gz \
+	-o /proyectos/home/hachepunto/BRCA_mtDNA_duos/VEP/strelka_all.somatic_merge_VEP.vcf \
+	--stats_file /proyectos/home/hachepunto/BRCA_mtDNA_duos/VEP/strelka_all.somatic_merge_vep.vcf_summary.html
+
+# Strelka passed.somatic.snvs merge
+
+perl variant_effect_predictor.pl \
+	--everything \
+	--cache \
+	--vcf \
+	--port 3337 \
+	-i /proyectos/home/hachepunto/BRCA_mtDNA_duos/strelka/strelka_passed.somatic_results/mergevcf/strelka_passed.somatic_merge.vcf.gz \
+	-o /proyectos/home/hachepunto/BRCA_mtDNA_duos/VEP/strelka_passed.somatic_merge_VEP.vcf \
+	--stats_file /proyectos/home/hachepunto/BRCA_mtDNA_duos/VEP/strelka_passed.somatic_merge_vep.vcf_summary.html
